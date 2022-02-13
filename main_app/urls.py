6cbe0ee2a954c2219211594b71ba3e5127544ec2
Smlcrp <smlcrp@gmail.com>
2022-02-13 16:21:55 -0500

    created full crud for duck model

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('ducks/', views.DuckList.as_view(), name='duck_list'),
     path('ducks/new/', views.DuckCreate.as_view(), name='duck_create'),
+    path('ducks/<int:pk>/', views.DuckDetail.as_view(), name='duck_detail'),
+    path('ducks/<int:pk>/update', views.DuckUpdate.as_view(), name="duck_update"),
+    path('ducks/<int:pk>/delete', views.DuckDelete.as_view(), name='duck_delete')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.urls import reverse
 from .models import Duck
 
 
@@ -25,3 +28,31 @@ class DuckList(TemplateView):
             context['ducks'] = Duck.objects.all()
             context['header'] = "All The Ducks"
         return context
+
+
+
+class DuckCreate(CreateView):
+    model = Duck
+    fields = ['name', 'img', 'bio']
+    template_name = 'duck_create.html'
+    def get_success_url(self):
+        return reverse('duck_detail', kwargs={'pk': self.object.pk})
+
+
+class DuckDetail(DetailView):
+    model = Duck
+    template_name = 'duck_detail.html'
+
+
+class DuckUpdate(UpdateView):
+    model = Duck
+    fields = ['name', 'img', 'bio']
+    template_name = 'duck_update.html'
+    def get_success_url(self):
+        return reverse('duck_detail', kwargs={'pk': self.object.pk})
+
+
+class DuckDelete(DeleteView):
+    model = Duck
+    template_name = 'duck_delete_confirmation.html'
+    success_url = '/ducks/'
